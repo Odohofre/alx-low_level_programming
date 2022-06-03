e4320e49738a20f682818462f4886721ceb03b86
Bright Odohofre <Bright Odohofre>
2022-06-02 17:30:00 -0700

    fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
  * main - the main function
  *
  * Return: 1 (success)
- * 
+ *
  */
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Kopa, \
-			2015-10-19\n");
+		2015-10-19\n", 59);
 	return (1);
 }
