73e33ac51aa448ecd8a1e098e2d7ce982132db2c
Bright Odohofre <Bright Odohofre>
2022-06-04 08:05:22 -0700

    add task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Thi is the intro to the function
+ *
+ * Description: This prints out numbers from 0 to 9
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
