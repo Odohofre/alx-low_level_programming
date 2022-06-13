098c03496f9db7eb7c1fd62f1361ffd2643f48fc
Bright Odohofre <Bright Odohofre>
2022-06-13 10:21:38 -0700

    Add task 4, 5, & 6

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: String
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = _strlen(s);
+
+	for (i = n; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
