2ad6b12b7c4625225d82a9e260475d390d941c93
Bright Odohofre <Bright Odohofre>
2022-06-13 08:51:00 -0700

    fix task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src)
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0')
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		dest[i] = src[i];
+		i++;
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
