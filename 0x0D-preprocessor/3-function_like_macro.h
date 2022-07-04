f425ac06ffda8d8cacf9608f0f8594b2aabb6f2f
Bright Odohofre <odohofrebright@gmail.com>
2022-07-04 08:30:38 -0700

    fix task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
+#ifndef DOUBLEGUARD
+#define DOUBLEGUARD
 
-#define ABS(x) (((x) < 0) ? (-x) : (x))
+#define ABS(x) ((x) < 0 ? ((x) * (-1)) : (x))
 
 #endif
