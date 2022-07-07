93b809cff3a9f679743cb431630893e751656a03
Bright Odohofre <odohofrebright@gmail.com>
2022-07-07 06:46:53 -0700

    fix betty errors task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,28 @@
 #include "variadic_functions.h"
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: parameters
+ * Return: Sum of parameters
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
-	
+
 	/* Declaring pointer to the argument list */
-	va_list ptr; 
+	va_list ptr;
 
 	if (n == 0)
-		return 0;
-	
+		return (0);
+
 	/* Initializing argument to the list pointer */
+	va_start(ptr, n);
 
-	va_start(ptr, n); 
-
-	for ( i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, int);
 
 	va_end(ptr);
 
-	return sum;
-
+	return (sum);
 }
