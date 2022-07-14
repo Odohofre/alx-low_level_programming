5eb0f04f3a1ace7920895dc25c02edaa579845dc
Odohofre <odohofrebright@gmail.com>
2022-07-14 18:49:02 +0100

    Add task 0, 1, 2 & update lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdlib.h> /* malloc */
+#include <stdio.h>	/* size_t, printf */
+#include <string.h> /* strdup */
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
