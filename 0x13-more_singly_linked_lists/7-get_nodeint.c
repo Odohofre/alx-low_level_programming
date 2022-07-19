1eb6e08b7204a88402ff2b3803e0cc2c05a7185d
Odohofre <odohofrebright@gmail.com>
2022-07-19 17:58:04 +0100

    Add the task 7-14

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *  get_nodeint_at_index - returns the nth node
+ *
+ * @head: pointer to list
+ * @index: index of the node
+ * Return: pointer to the nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	listint_t *current = head;
+
+	while ((i < index) && (current != NULL))
+	{
+		current = current->next;
+		i++;
+	}
+	
+	return (current ? current : NULL);
+}
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_listint - Adds the data in a linked list
+ * 
+ * @head: first node in a list
+ * Return: sum of all the data in the list
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	listint_t *current = head;
+
+	while(current)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
