40daddec55f358b3276b6d72ad6d362e48a0ce88
Odohofre <odohofrebright@gmail.com>
2022-09-03 23:20:46 +0100

    fix task 2 & 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -4,7 +4,7 @@
  *
  * @head: pointer  to the head of the list
  * @n: integer
- * Return:
+ * Return: the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
