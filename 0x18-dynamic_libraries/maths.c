039103156afb61e0a443cf981ffcce59a1e9c309
Odohofre <odohofrebright@gmail.com>
2022-09-13 15:01:20 +0100

    fix errors in task 2

--- 0x18-dynamic_libraries/maths.c
+++ 0x18-dynamic_libraries/maths.c
@@ -4,7 +4,7 @@ int main()
 {
 	/* Variable declation */
 	int firstNumber, secondNumber;
-	int sum, difference, product, modulo;
+	int sum, difference, product, modul;
 	float quotient;
 
 	/* Taking input from user and storing it in firstNumber and secondNumber */
@@ -26,13 +26,58 @@ int main()
 	quotient = division(firstNumber, secondNumber);
 
 	/* Remainder when firstNumber is divided by secondNumber*/
-	modulo = mod(firstNumber, secondNumber);
+	modul = mod(firstNumber, secondNumber);
 
 	printf("\nSum = %d", sum);
 	printf("\nDifference  = %d", difference);
 	printf("\nMultiplication = %d", product);
 	printf("\nDivision = %.3f", quotient);
-	printf("\nRemainder = %d", modulo);
+	printf("\nRemainder = %d", modul);
 
 	return 0;
+}
+
+/*
+* Function to add two numbers
+*/
+int addition(int num1, int num2){
+    int sum;
+    sum = num1 + num2;
+    return sum;
+}
+/*
+* Function to subtract two numbers
+*/
+int subtraction(int num1, int num2){
+    int difference;
+    difference = num1 - num2;
+    return difference;
+}
+
+/*
+* Function to multiply two numbers
+*/
+int multiplication(int num1, int num2){
+    int product;
+    product = num1 * num2;
+    return product;
+}
+
+/*
+* Function to divide two numbers
+*/
+float division(int num1, int num2){
+    float quotient;
+    quotient = (float)num1 / num2;
+    return quotient;
+}
+
+/*
+ * Function to return remainder when one
+ * number is divided by other number
+ */
+int mod(int num1, int num2){
+    int modulo;
+    modulo = num1 % num2;
+    return modulo;
 }
